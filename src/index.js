e54f9b8ff7c7b8c21624a29f914391c2fcb71128
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-12-25 16:41:38 +0200

    basename corrected

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/tt-oksi-fe">
+        <BrowserRouter basename="/TT-OKSI-FE">
           <App />
         </BrowserRouter>
       </PersistGate>
