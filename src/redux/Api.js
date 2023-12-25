b987014207793c78073decba761d039ac1481798
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-12-25 16:25:44 +0200

    url correct

--- src/redux/Api.js
+++ src/redux/Api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:7373",
+    baseUrl: "https://tt-oksi-be.onrender.com",
   }),
   tagTypes: ["User"],
   endpoints: (builder) => ({
